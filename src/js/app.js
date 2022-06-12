45022b97037cd042c54c9c52495f154187989520
BGil1011 <bryangil1112@hotmail.com>
2022-06-12 17:30:53 -0400

    up

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-document.querySelector("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",()=>{
   inicio();
 });
 
 function inicio(){
-  console.log("TODO OK");
+  
+}
+
+const ingreso = document.querySelector('input[type="submit"]');
+
+ingreso.addEventListener("click",logear)
+
+function logear(e){
+  e.preventDefault();
+  console.log("Ingresando...");
 }
